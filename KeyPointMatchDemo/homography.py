9d7b50c1b570fee00e6fce47141657139011fae8
Samuel Tabor <samuelctabor@gmail.com>
2021-05-08 18:50:50 +0100

    Add option to use ORB.

--- KeyPointMatchDemo/homography.py
+++ KeyPointMatchDemo/homography.py
@@ -2,26 +2,35 @@ import cv2
 import numpy as np
 from timeit import default_timer as timer
 
-img = cv2.imread("Scotland-lowres.png", cv2.IMREAD_GRAYSCALE)  # queryiamge
+img  = cv2.imread("Scotland-lowres.png", cv2.IMREAD_GRAYSCALE)  # queryiamge
+img2 = cv2.imread("Scotland2-lowres.png", cv2.IMREAD_GRAYSCALE)
 
 n_kp = 30;
 
+FLANN_INDEX_KDTREE = 1  # bug: flann enums are missing
+FLANN_INDEX_LSH    = 6
+FEATURE_TYPE = "ORB"
 
-# Features
-sift = cv2.SIFT_create(n_kp)
-kp_image, desc_image = sift.detectAndCompute(img, None)
+print("Starting keypoint search")
+if FEATURE_TYPE == "SIFT":
+    sift = cv2.SIFT_create(n_kp)
+    kp_image, desc_image = sift.detectAndCompute(img, None)
+    kp_image2, desc_img2 = sift.detectAndCompute(img2, None)
+    
+    index_params = dict(algorithm=0, trees=5)
+else:
+    orb = cv2.ORB_create(n_kp)
+    kp_image, desc_image  = orb.detectAndCompute(img, None)
+    kp_image2, desc_img2 = orb.detectAndCompute(img2, None)
+    
+    index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                   multi_probe_level = 1) #2
 
-
-# Feature matching
-index_params = dict(algorithm=0, trees=5)
 search_params = dict()
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-img2 = cv2.imread("Scotland2-lowres.png", cv2.IMREAD_GRAYSCALE)
-
-
-print("Starting keypoint search")
-kp_image2, desc_img2 = sift.detectAndCompute(img2, None)
 print("Starting keypoint match")
 start=timer()
 matches = flann.knnMatch(desc_image, desc_img2, k=2)
